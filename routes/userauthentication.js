8af6a03a9006e95dcbcccc9a1883aa8f74c41710
yingqiaojian <qiaoying@hotmail.com>
2015-06-15 19:54:59 -0400

    added twitter-api-node express client there

--- routes/userauthentication.js
+++ routes/userauthentication.js
@@ -6,13 +6,53 @@ var express = require('express');
 
 var router = express.Router();
 
+var oauth = require('oauth');
+
+var OAuth2 = oauth.OAuth2;
+
+var twitterConsumerKey = 'i1DJVo1bpMqlyQM7wMpnChE7L';
+
+var twitterConsumerSecret = '9uqEcSwSjHGMHYhcmNc4yKd0VzMzz5hmJl5hTJ98qV53GfQjrc';
+
+var accessToken;
+
 router.get('/api', function(req, res, next){
     res.send('twitter user authentication with /authentication?account={twitter_account}');
 
 });
 
-router.post('/authentication',function(req,res,next){
+router.get('api/appauth',function(req,res,next){
 
+    var oauth2 = new OAuth2(
+        twitterConsumerKey,
+        twitterConsumerSecret,
+        'https://api.twitter.com/',
+        null,
+        'oauth2/token',
+        null);
+
+    oauth2.getOAuthAccessToken(
+        '',
+        {'grant_type':'client_credentials'},
+        function (e, access_token, refresh_token, results){
+            console.log('bearer: ',access_token);
+            oauth2.get('protected url',
+                access_token, function(e,data,res) {
+                    if (e) return callback(e, null);
+                    if (res.statusCode!=200)
+                        return callback(new Error(
+                            'OAuth2 request failed: '+
+                            res.statusCode),null);
+                    try {
+                        data = JSON.parse(data);
+                    }
+                    catch (e){
+                        return callback(e, null);
+                    }
+                    return callback(e, data);
+                });
+            accessToken = access_token;
+        });
 
 });
 
